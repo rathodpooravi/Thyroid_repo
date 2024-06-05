f030f4dbbb771d7f1a2740520fe672e48be40486
rathodpooravi <rathodpooravi.2@gmail.com>
2024-06-05 16:50:11 +0530

    Update thy_config.py

--- thy_config.py
+++ thy_config.py
@@ -1,5 +1,5 @@
-MODEL_FILE_PATH = r'F:\DataScience\Practice\Machine Learning\Second_API\files\Logistic_Model.pkl'
-JSON_FILE_PATH = r'F:\DataScience\Practice\Machine Learning\Second_API\files\project_data.json'
+MODEL_FILE_PATH = r'F:/DataScience/Practice/Machine Learning/Second_API/files/Logistic_Model.pkl'
+JSON_FILE_PATH = r'F:/DataScience/Practice/Machine Learning/Second_API/files/project_data.json'
 
 
 # MODEL_FILE_PATH = 'Logistic_Model.pkl'
