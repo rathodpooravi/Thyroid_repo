6a909edb183f53e122ec221dbaf2e5af50c69048
rathodpooravi <rathodpooravi.2@gmail.com>
2024-06-05 16:53:05 +0530

    Update thy_config.py

--- thy_config.py
+++ thy_config.py
@@ -1,8 +1,8 @@
-MODEL_FILE_PATH = r'F:/DataScience/Practice/Machine Learning/Second_API/files/Logistic_Model.pkl'
-JSON_FILE_PATH = r'F:/DataScience/Practice/Machine Learning/Second_API/files/project_data.json'
+# MODEL_FILE_PATH = r'F:/DataScience/Practice/Machine Learning/Second_API/files/Logistic_Model.pkl'
+# JSON_FILE_PATH = r'F:/DataScience/Practice/Machine Learning/Second_API/files/project_data.json'
 
 
-# MODEL_FILE_PATH = 'Logistic_Model.pkl'
-# JSON_FILE_PATH = 'project_data.json'
+MODEL_FILE_PATH = 'Logistic_Model.pkl'
+JSON_FILE_PATH = 'project_data.json'
 
 PORT_NUMBER = 8080
